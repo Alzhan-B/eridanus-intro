9651f8335089ba249cf1c2c825397fee06ce96f7
Alzhan-B <115260318+Alzhan-B@users.noreply.github.com>
2023-04-02 23:19:14 -0400

    Revert "Lesson 4.5"

--- js/index.js
+++ js/index.js
@@ -3,19 +3,19 @@ const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
 
-copyright.innerHTML = ` Alzhan Braliyev &copy ${thisYear}`;
+copyright.innerHTML = ` Alzhan B &copy ${thisYear}`;
 footer.appendChild(copyright);
 
-//Putting skills in HTML in ul and li
-const skills = ["Java Script", "HTML", "CSS", "Node.js", "Mocha", "React", "MySQL"]; 
+const skills = ["Java Script", "HTML", "CSS", "Something else"]; 
 const skillsSection = document.getElementById("skills");
+
 const skillsList = skillsSection.querySelector("ul");
 for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
-// Creating Messages form with input fields
+// Messages form
 const messageForm = document.getElementsByName("leave_message");
 messageForm.item(0).addEventListener("submit", (event) => { 
     event.preventDefault();  
@@ -27,28 +27,21 @@ messageForm.item(0).addEventListener("submit", (event) => {
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
     const newMessage = document.createElement("li");
-   
     newMessage.innerHTML = `<a href="mailto:${emailInput}">${nameInput}</a> <span> ${messageInput} </span>`;
     messageList.appendChild(newMessage);
 
-//Remove button and disappearing Messages section
+    //Remove button
     const removeButton = document.createElement("button");
     removeButton.innerText = "Remove";
     removeButton.type = "button";
-
     removeButton.addEventListener("click", function() {
         const entry = removeButton.parentNode;
         entry.remove();
-        if (messageList.childElementCount === 0) {
-            messageSection.style.display = "none";
-        }
     });
-
-    messageSection.style.display = "block"
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
-//Creating Edit button and new input prompt
+    //Edit button
     const editButton = document.createElement("button");
     editButton.innerText = "Edit";
     editButton.type = "button";
@@ -60,6 +53,13 @@ messageForm.item(0).addEventListener("submit", (event) => {
     })
     newMessage.appendChild(editButton);
     messageList.appendChild(newMessage);
+
+    // const messagesHeader = messageSection.getElementsByTagName("h2");
+    // if (!messages.hasChildNodes()) {
+    //     messageSection.style.display = "none";
+    //     messagesHeader.style.display = "none";
+    //     document.getElementById("messages").style.display = "none";
+    // }
     
     document.querySelector('form').reset();
 });

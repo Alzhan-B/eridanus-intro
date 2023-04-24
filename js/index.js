13cb00d207d79475831533c57280dfdd4da6263c
Alzhan-B <alzhan.braliyev@gmail.com>
2023-04-24 01:48:53 -0400

    Final

--- js/index.js
+++ js/index.js
@@ -3,18 +3,43 @@ const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
 
-copyright.innerHTML = ` Alzhan Braliyev &copy ${thisYear}`;
+copyright.innerHTML = ` Alzhan Braliyev &copy ${thisYear} `;
 footer.appendChild(copyright);
 
 // -- Putting skills in HTML in ul and li
-const skills = ["Java Script", "HTML", "CSS", "Node.js", "Mocha", "React", "MySQL"]; 
-const skillsSection = document.getElementById("skills");
-const skillsList = skillsSection.querySelector("ul");
-for (let i = 0; i < skills.length; i++) {
+// const skills = ["Java Script", "HTML", "CSS", "Node.js", "Mocha", "React", "MySQL"]; 
+// const skillsSection = document.getElementById("skills");
+// const skillsList = skillsSection.querySelector("ul");
+// for (let i = 0; i < skills.length; i++) {
+//     let skill = document.createElement("li");
+//     skill.innerText = skills[i];
+//     skillsList.appendChild(skill);
+// }
+const skills = [
+    { name: "Java Script", link: "https://www.javascript.com/" },
+    { name: "HTML", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+    { name: "CSS", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+    { name: "Node.js", link: "https://nodejs.org/en" },
+    { name: "Mocha", link: "https://mochajs.org/" },
+    { name: "React", link: "https://reactnative.dev/" },
+    { name: "MySQL", link: "https://www.mysql.com/" }
+  ];
+  
+  const skillsSection = document.getElementById("skills");
+  const skillsList = skillsSection.querySelector("ul");
+  
+  for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
-    skill.innerText = skills[i];
+    let link = document.createElement("a");
+    
+    link.href = skills[i].link;
+    link.target = "_blank";
+    link.textContent = skills[i].name;
+  
+    skill.appendChild(link);
     skillsList.appendChild(skill);
-}
+  }
+
 // -- Creating Messages form with input fields
 const messageForm = document.getElementsByName("leave_message");
 messageForm.item(0).addEventListener("submit", (event) => { 
@@ -35,6 +60,7 @@ messageForm.item(0).addEventListener("submit", (event) => {
     const removeButton = document.createElement("button");
     removeButton.innerText = "Remove";
     removeButton.type = "button";
+    removeButton.classList.add("RemoveButton");
 
     removeButton.addEventListener("click", function() {
         const entry = removeButton.parentNode;
@@ -52,6 +78,8 @@ messageForm.item(0).addEventListener("submit", (event) => {
     const editButton = document.createElement("button");
     editButton.innerText = "Edit";
     editButton.type = "button";
+    editButton.classList.add("EditButton");
+
     editButton.addEventListener("click", () => {
         let editMessage = prompt("Enter your new message:", `${messageInput}`);
         newMessage.innerHTML = `<a href="mailto:${emailInput}">${nameInput}</a> <span> ${editMessage} </span>`;
@@ -63,25 +91,25 @@ messageForm.item(0).addEventListener("submit", (event) => {
     
     document.querySelector("form").reset();
 
-    removeButton.style.backgroundColor = '#ff0545';
-    removeButton.style.color = '#fff';
-    removeButton.style.border = 'none';
-    removeButton.style.borderRadius = '4px';
-    removeButton.style.padding = '8px 16px';
-    removeButton.style.marginRight = '8px';
-    removeButton.style.cursor = 'pointer';
-    removeButton.style.fontSize = 'small';
-    removeButton.style.justifyContent = "center"
+    // removeButton.style.backgroundColor = 'darkred';
+    // removeButton.style.color = '#fff';
+    // removeButton.style.border = 'none';
+    // removeButton.style.borderRadius = '4px';
+    // removeButton.style.padding = '8px 16px';
+    // removeButton.style.marginRight = '8px';
+    // removeButton.style.cursor = 'pointer';
+    // removeButton.style.fontSize = 'small';
+    // removeButton.style.justifyContent = "center"
 
-    editButton.style.backgroundColor = '#wa745';
-    editButton.style.color = '#fff';
-    editButton.style.border = 'none';
-    editButton.style.borderRadius = '4px';
-    editButton.style.padding = '8px 16px';
-    editButton.style.marginRight = '8px';
-    editButton.style.cursor = 'pointer';
-    editButton.style.fontSize = 'small';
-    editButton.style.justifyContent = "center"
+    // editButton.style.backgroundColor = '#wa745';
+    // editButton.style.color = '#fff';
+    // editButton.style.border = 'none';
+    // editButton.style.borderRadius = '4px';
+    // editButton.style.padding = '8px 16px';
+    // editButton.style.marginRight = '8px';
+    // editButton.style.cursor = 'pointer';
+    // editButton.style.fontSize = 'small';
+    // editButton.style.justifyContent = "center"
 });
 
 // Function for fixing the date
